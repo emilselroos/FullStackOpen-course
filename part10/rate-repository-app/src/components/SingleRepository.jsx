fbc3ee61fd077bd3d3cdc4f902025d311e11ef15
Emil Selroos <emil.selroos@gmail.com>
2021-12-12 20:36:21 +0200

    Commit part10 answers (3/4)

---
+++ part10/rate-repository-app/src/components/SingleRepository.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+import RepositoryItem from './RepositoryItem';
+
+const SingleRepository = () => {
+
+    
+
+    return (
+        <View>
+            <RepositoryItem
+                item={}
+            />
+        </View>
+    );
+}
+
+export default SingleRepository;
